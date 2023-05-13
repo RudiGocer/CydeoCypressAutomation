8fed32cbb7831bca3f9ef66848824059c046a3b6
Rudi_Cydeo <rudigocer@gmail.com>
2023-05-13 14:23:17 -0400

    Added more test

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -19,4 +19,17 @@ describe('Context: My First Test', () => {
         cy.visit('/registration_form');
         
     })
+    it('Test 2', () => {
+        expect(false).to.equal(false);
+    })
+    it('Test 3', () => {
+        expect(false).not.to.equal(true);
+    })
+    it('Test 4', () => {
+        expect(5).to.equal(5);
+    })
+    it('Test 5', () => {
+        expect(true).to.equal('5'==5);
+
+    })
 })
